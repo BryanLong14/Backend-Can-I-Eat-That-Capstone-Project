83e0a5dbe8ee9e476d39379d21deb82335419ff8
Bryan Long <BryanLong1@gmail.com>
2018-03-21 10:21:19 -0600

    Added routes and queries

--- api/foods.js
+++ api/foods.js
@@ -5,9 +5,22 @@ const router = express.Router();
 const queries = require("../db/queries");
 
 // All routes will be prepended by /api/foods/
-router.get("/", async (req, res) => {
-    const foods = await queries.getAll();
-    res.json(foods);
+router.get('/', (req, res, next) => {
+  queries
+    .getAll()
+    .then(foods => {
+      res.json(foods);
+    })
+    .catch(next);
+});
+
+router.get("/:id", (req, res, next) => {
+    queries
+    .getOne(req.params.id)
+    .then(foods => {
+        foods ? res.json(foods) : response.sendStatus(404);
+    })
+    .catch(next);
 });
 
 module.exports = router;
--- db/queries.js
+++ db/queries.js
@@ -3,5 +3,19 @@ const db = require("./index");
 module.exports = {
     getAll() {
         return db("food_to_avoid");
+    },
+    getOne(id) {
+        return db("food_to_avoid").where('id', id).first();
+    },
+    create(food) {
+        return db("food_to_avoid").insert(food, 'id').then(ids => {
+             return ids[0];
+            });
+    },
+    update(id, food) {
+        return db("food_to_avoid").where('id', id).update(food);        
+    },
+    delete(id){
+        return db("food_to_avoid").where('id', id).del();       
     }
 };

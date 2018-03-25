61c6b91a40b93bc8be76308b9878bb2101603a5a
Bryan Long <BryanLong1@gmail.com>
2018-03-25 11:47:58 -0600

    API routing updated

--- api/scan.js
+++ api/scan.js
@@ -7,11 +7,9 @@ const fetch = require("isomorphic-fetch");
 
 // All routes will be prepended by /api/foods/
 router.get("/:upc?", (req, res, next) => {
-  var options = {
-    method: "POST",
-    headers: { "Cache-Control": "no-cache", "Content-Type": "application/x-www-form-urlencoded", Accept: "application/json" },
-    body: new URLSearchParams({ login: "BLong", password: "1lvPhish", upc: req.body.upc || req.query.upc || req.params.upc })
-  };
+  var options = { method: "POST", headers: { "Cache-Control": "no-cache", "Content-Type": "application/x-www-form-urlencoded", Accept: "application/json" }, body: new URLSearchParams( // body: new URLSearchParams({ login: "BLong", password: "1lvPhish", upc: req.body.upc || req.query.upc || req.params.upc })
+      { login: "canieatthat", password: "CanIEatThatpw?!1", upc: req.body.upc || req.query.upc || req.params.upc }
+    ) };
   fetch("https://api.foodfacts.com/ci/api/foodfacts/food_find_product_by_upc", options)
     .then(res => res.json())
     .then(data => {
